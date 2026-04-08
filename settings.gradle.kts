e9eb54c4b7e0bec34751ca33fddb4eed23a98161
kkk <190509143+luanmuc@users.noreply.github.com>
2026-04-08 10:53:57 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,40 +1,19 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 官方依赖仓库，确保能拉取所有Android官方依赖
         google()
         mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://api.xposed.info/")
-        maven("https://raw.githubusercontent.com/fankes/maven-repository/main/repository/releases")
     }
 }
-plugins {
-    id("com.highcapable.gropify") version "1.0.1"
-}
-gropify {
-    global {
-        common {
-            includeKeys(
-                "GITHUB_CI_COMMIT_ID",
-                "^project\\..*\$".toRegex()
-            )
-            permanentKeyValues("GITHUB_CI_COMMIT_ID" to "")
-            locations(GropifyLocation.RootProject, GropifyLocation.SystemEnv)
-        }
-        android {
-            includeKeys("GITHUB_CI_COMMIT_ID")
-            // 手动指定类型，防止一些特殊 "COMMIT ID" 被生成为数值
-            keyValuesRules("GITHUB_CI_COMMIT_ID" to ValueRule(String::class))
-        }
-    }
-    rootProject { common { isEnabled = false } }
-}
+
 rootProject.name = "TSBattery"
 include(":app")
