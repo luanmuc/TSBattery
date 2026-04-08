c1a173e3d6ef3faa8c27e3d0431a01700c15d785
kkk <190509143+luanmuc@users.noreply.github.com>
2026-04-08 12:19:23 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // 官方依赖仓库，确保能拉取所有Android官方依赖
         google()
         mavenCentral()
     }
